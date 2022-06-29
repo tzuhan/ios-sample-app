4e3000d2cc4ef9822be83caae2ae0eb7d193c825
tzuhan <stoola08@gmail.com>
2022-06-29 14:38:17 +0800

    Initial Commit

---
+++ ios-sample-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-sample-app
+//
+//  Created by Tzu-Han Hsu on 2022/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-sample-app/ios_sample_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sample_appApp.swift
+//  ios-sample-app
+//
+//  Created by Tzu-Han Hsu on 2022/6/29.
+//
+
+import SwiftUI
+
+@main
+struct ios_sample_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
